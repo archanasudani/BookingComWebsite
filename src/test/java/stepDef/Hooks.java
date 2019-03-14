c316cfdfcff37306bb495fbac6bd6f090926d4ec
archana sudani <archanasudani@gmail.com>
2019-03-14 20:23:50 +0000

    my first commmit

---
+++ src/test/java/stepDef/Hooks.java
@@ -0,0 +1,39 @@
+package stepDef;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
+import utilities.ConfigFileReader;
+
+import java.util.concurrent.TimeUnit;
+
+import static stepDef.Hooks.driver;
+
+public class Hooks {
+    public static WebDriver driver;
+    public static ChromeOptions options;
+    public static ConfigFileReader cfr = new ConfigFileReader();
+
+    @Before
+    public void beforeTest(){
+        options=new ChromeOptions();
+        options.addArguments("--start-maximized");  // window maximizing
+        options.addArguments("--disable-notifications"); //disable popup notifications
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\personal\\BookingComWebsite\\chromedriver.exe");
+        driver = new ChromeDriver(options);
+        // driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);//implicit wait
+        PageFactory.initElements(driver, BookingElements.class);
+
+    }
+
+    @After
+    public void afterTest(){
+        //  driver.quit();
+
+    }
+}
